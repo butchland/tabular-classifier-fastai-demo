99bfca4cd4b35aad48a0f3b827a26a5ffb199fa9
Butch Landingin <butchland@gmail.com>
2019-05-06 00:45:59 +0800

    Add configurability for model url and refresh

--- app/config.py
+++ app/config.py
@@ -20,8 +20,17 @@ api_response_key_name = 'result'
 # configuration for image learner pkl file stored in cloud
 # see https://course.fast.ai/deployment_render.html for instructions to generate download urls
 # for model files (.pkl) shared on google drive and dropbox
-model_file_download_url = 'https://drive.google.com/uc?export=download&id=1-OXLsznNXsAvexiLHaBMoEQ4-N6N-t_4'
+# model_file_download_url = 'https://drive.google.com/uc?export=download&id=1-OXLsznNXsAvexiLHaBMoEQ4-N6N-t_4'
+# https://drive.google.com/uc?export=download&id=1vRlKo_mIsQgyU4LkE39xzyqMfG5KGN3J
+model_file_download_url = 'https://drive.google.com/uc?export=download&id=1vRlKo_mIsQgyU4LkE39xzyqMfG5KGN3J'
+if 'MODEL_URL' in os.environ:
+    model_file_download_url = os.environ['MODEL_URL']
+
 model_file_name = 'export.pkl'
+
+refresh_model = False
+if 'REFRESH_MODEL' in os.environ:
+    refresh_model = os.environ['REFRESH_MODEL'].lower()  in ('true','1','yes','y')
 
 # configuration for prediction handler
 # change this custom.petbreed_prediction_handler into 
