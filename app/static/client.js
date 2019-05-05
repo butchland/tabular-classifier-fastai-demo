8a70c6275225487bd6f4f20efb6bba9281a0323e
Butch Landingin <butchland@gmail.com>
2019-05-06 00:50:26 +0800

    Update webapp for tabular classifier

--- app/static/client.js
+++ app/static/client.js
@@ -1,7 +1,8 @@
 var el = x => document.getElementById(x);
 var prediction_api_path = '/analyze';
 var api_response_key_name = 'result';
-
+var DEBUG = true;
+/*
 function showPicker(inputId) { el('file-input').click(); }
 
 function showPicked(input) {
@@ -13,11 +14,21 @@ function showPicked(input) {
     }
     reader.readAsDataURL(input.files[0]);
 }
+*/
 
 function analyze() {
-    var uploadFiles = el('file-input').files;
-    if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
-
+    //var uploadFiles = el('file-input').files;
+    //if (uploadFiles.length != 1) alert('Please select 1 file to analyze!');
+    var age = el('form-age').value;
+    var fnlwgt = el('form-fnlwgt').value;
+    var education_num = el('form-education-num').value;
+    var workclass = el('form-workclass').value;
+    var education = el('form-education').value;
+    var marital_status = el('form-marital-status').value;
+    var occupation = el('form-occupation').value;
+    var relationship = el('form-relationship').value;
+    var race = el('form-race').value;
+    
     el('analyze-button').innerHTML = 'Analyzing...';
     var xhr = new XMLHttpRequest();
     var loc = window.location
@@ -31,8 +42,23 @@ function analyze() {
         el('analyze-button').innerHTML = 'Analyze';
     }
 
-    var fileData = new FormData();
-    fileData.append('file', uploadFiles[0]);
-    xhr.send(fileData);
+    var formData = new FormData();
+    formData.append('age', age);
+    formData.append('fnlwgt', fnlwgt);
+    formData.append('education-num', education_num);
+    formData.append('workclass', workclass);
+    formData.append('education', education);
+    formData.append('marital-status', marital_status);
+    formData.append('occupation', occupation);
+    formData.append('relationship', relationship);
+    formData.append('race', race);
+    if (DEBUG) {
+        console.log('formData entries:')
+        for (var pair of formData.entries()) {
+            console.log(pair[0]+ ', ' + pair[1]); 
+        }    
+    }
+ 
+    xhr.send(formData);
 }
 
